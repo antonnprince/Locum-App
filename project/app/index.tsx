f5487753e54b965310858e330a490334c3b4b311
antonnprince <antonprince95@gmail.com>
2025-05-13 16:38:47 +0530

    verde

--- project/app/index.tsx
+++ project/app/index.tsx
@@ -1,12 +1,8 @@
 import { Redirect } from "expo-router";
-import React, { useEffect, useState } from "react";
 import "./global.css";
-import { Session } from "@supabase/supabase-js";
-import { supabase } from "@/utils/supabase";
-import { Image } from "react-native";
 import Loader from "./components/Loader";
 import { useAuth } from "@/hooks/useSessionContext";
-// import loaderComponent from "../assets/loader.svg"
+
 
 
 export default function Index() {
